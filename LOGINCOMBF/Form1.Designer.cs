d118f5e9849c3d7bc3b4a510b34868beca30d414
RAQUEL <RAQUEL@local.rp>
2022-04-18 20:13:39 -0300

    Envio dos arquivos

---
+++ LOGINCOMBF/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace LOGINCOMBF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1_LOGIN = new System.Windows.Forms.TextBox();
+            this.textBox2_SENHA = new System.Windows.Forms.TextBox();
+            this.button1_logar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "LOGIN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(30, 156);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SENHA";
+            // 
+            // textBox1_LOGIN
+            // 
+            this.textBox1_LOGIN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_LOGIN.Location = new System.Drawing.Point(149, 70);
+            this.textBox1_LOGIN.Name = "textBox1_LOGIN";
+            this.textBox1_LOGIN.Size = new System.Drawing.Size(140, 31);
+            this.textBox1_LOGIN.TabIndex = 2;
+            // 
+            // textBox2_SENHA
+            // 
+            this.textBox2_SENHA.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_SENHA.Location = new System.Drawing.Point(149, 156);
+            this.textBox2_SENHA.Name = "textBox2_SENHA";
+            this.textBox2_SENHA.Size = new System.Drawing.Size(140, 31);
+            this.textBox2_SENHA.TabIndex = 3;
+            // 
+            // button1_logar
+            // 
+            this.button1_logar.Location = new System.Drawing.Point(80, 215);
+            this.button1_logar.Name = "button1_logar";
+            this.button1_logar.Size = new System.Drawing.Size(186, 69);
+            this.button1_logar.TabIndex = 4;
+            this.button1_logar.Text = "LOGAR";
+            this.button1_logar.UseVisualStyleBackColor = true;
+            this.button1_logar.Click += new System.EventHandler(this.button1_logar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 327);
+            this.Controls.Add(this.button1_logar);
+            this.Controls.Add(this.textBox2_SENHA);
+            this.Controls.Add(this.textBox1_LOGIN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1_LOGIN;
+        private System.Windows.Forms.TextBox textBox2_SENHA;
+        private System.Windows.Forms.Button button1_logar;
+    }
+}
+
---
+++ LOGINCOMBF/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace LOGINCOMBF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /*    String conexao = "Server=locahost;Database=sistema;Uid=root;password=1234;SsLmODE=none";
+            var Conection = new MySqlConnection(conexao);
+            var comand = Conection.CreateCommand();
+            MySqlCommand query = new MySqlCommand("'Select count(*) from usuario where LOGIN ='" + textBox1_LOGIN + "' and SENHA '" + textBox2_SENHA + " ' ", Conection);
+            Conection.Open();
+            DataTable datatable = new DataTable();
+            MySqlDataAdapter da = new MySqlDataAdapter(query);
+            da.Fill(datatable);
+
+            foreach(DataRow list in datatable.Rows)
+            {
+
+            }
+            Conection.Close();
+            MessageBox.Show("logado"); */
+        }
+
+        private void button1_logar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection mConn = new
+            MySqlConnection("server=localhost;database=sistema;uid=root;pwd=1234");
+
+            mConn.Open();
+
+
+            MySqlCommand comando = new MySqlCommand("select * from USUARIO where LOGIN= '" + textBox1_LOGIN.Text + "' and SENHA= '" + textBox2_SENHA.Text + "';", mConn);
+            comando.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            MySqlDataAdapter a = new MySqlDataAdapter(comando);
+            a.Fill(dt);
+
+            short i = Convert.ToInt16(dt.Rows.Count.ToString());
+            
+      
+                if (i > 0)
+                {
+                    MessageBox.Show("Login válido");
+                    this.Dispose();
+                }
+                else
+                {
+                    MessageBox.Show("Login inválido");
+                    this.Dispose();
+
+                }
+
+                mConn.Close();
+           
+
+         
+        }
+    }
+   
+}
