ca13a94a23201ec32c5ac683204af02f9414336e
Raquel Gonçalves <53949793+RaquelETEC@users.noreply.github.com>
2022-06-11 23:25:55 -0300

    ULTIMA VERSAO DA BIA

---
+++ SISTEMATESTE/sistemaclube/sistemaclube/frlogin.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace sistemaclube
+{
+    partial class frlogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frlogin));
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2_SENHA = new System.Windows.Forms.TextBox();
+            this.textBox1_LOGIN = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1_logar = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label3.Location = new System.Drawing.Point(967, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(183, 30);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Login de usuário";
+            // 
+            // textBox2_SENHA
+            // 
+            this.textBox2_SENHA.Font = new System.Drawing.Font("Segoe UI Symbol", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2_SENHA.Location = new System.Drawing.Point(896, 344);
+            this.textBox2_SENHA.Name = "textBox2_SENHA";
+            this.textBox2_SENHA.PasswordChar = '*';
+            this.textBox2_SENHA.Size = new System.Drawing.Size(326, 35);
+            this.textBox2_SENHA.TabIndex = 10;
+            // 
+            // textBox1_LOGIN
+            // 
+            this.textBox1_LOGIN.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1_LOGIN.Location = new System.Drawing.Point(896, 272);
+            this.textBox1_LOGIN.Name = "textBox1_LOGIN";
+            this.textBox1_LOGIN.Size = new System.Drawing.Size(326, 31);
+            this.textBox1_LOGIN.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(894, 316);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Senha:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(891, 238);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "E-mail:";
+            // 
+            // button1_logar
+            // 
+            this.button1_logar.BackColor = System.Drawing.Color.Transparent;
+            this.button1_logar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1_logar.FlatAppearance.BorderSize = 2;
+            this.button1_logar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1_logar.Font = new System.Drawing.Font("Segoe UI Symbol", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1_logar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1_logar.Location = new System.Drawing.Point(972, 436);
+            this.button1_logar.Name = "button1_logar";
+            this.button1_logar.Size = new System.Drawing.Size(186, 69);
+            this.button1_logar.TabIndex = 12;
+            this.button1_logar.Text = "Entrar";
+            this.button1_logar.UseVisualStyleBackColor = false;
+            this.button1_logar.Click += new System.EventHandler(this.button1_logar_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackgroundImage = global::sistemaclube.Properties.Resources.Clube_de_tênis_fundo;
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(0, 1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(746, 725);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Transparent;
+            this.panel2.BackgroundImage = global::sistemaclube.Properties.Resources.Design_sem_nome__4_;
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Location = new System.Drawing.Point(114, 137);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(508, 478);
+            this.panel2.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(323, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(162, 65);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "TÊNIS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label5.Location = new System.Drawing.Point(222, 285);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 40);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "DE";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(18, 285);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(174, 65);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "CLUBE";
+            // 
+            // frlogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Honeydew;
+            this.ClientSize = new System.Drawing.Size(1279, 619);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button1_logar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2_SENHA);
+            this.Controls.Add(this.textBox1_LOGIN);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frlogin";
+            this.Text = "Clube de tênis";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.frlogin_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2_SENHA;
+        private System.Windows.Forms.TextBox textBox1_LOGIN;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1_logar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ SISTEMATESTE/sistemaclube/sistemaclube/frzelador.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace sistemaclube
+{
+    partial class frzelador
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_reserva = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // bt_reserva
+            // 
+            this.bt_reserva.BackColor = System.Drawing.Color.Transparent;
+            this.bt_reserva.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.bt_reserva.FlatAppearance.BorderSize = 0;
+            this.bt_reserva.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bt_reserva.Font = new System.Drawing.Font("Segoe UI Symbol", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_reserva.ForeColor = System.Drawing.Color.White;
+            this.bt_reserva.Location = new System.Drawing.Point(840, 13);
+            this.bt_reserva.Name = "bt_reserva";
+            this.bt_reserva.Size = new System.Drawing.Size(158, 32);
+            this.bt_reserva.TabIndex = 0;
+            this.bt_reserva.Text = "Reserva de quadras";
+            this.bt_reserva.UseVisualStyleBackColor = false;
+            this.bt_reserva.Click += new System.EventHandler(this.bt_reserva_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(775, 477);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(216, 86);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "TÊNIS";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(654, 484);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 50);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "DE";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(380, 477);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(230, 86);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "CLUBE";
+            // 
+            // frzelador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::sistemaclube.Properties.Resources.clube_4;
+            this.ClientSize = new System.Drawing.Size(1300, 633);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.bt_reserva);
+            this.Name = "frzelador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.WindowsDefaultBounds;
+            this.Text = "Clube de tênis";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button bt_reserva;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
